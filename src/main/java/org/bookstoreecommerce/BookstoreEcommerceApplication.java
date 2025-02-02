90fea736bfbf26742ac41dd2dd282fd992624852
shobikan <shobikanv.21@uom.lk>
2025-02-03 01:51:33 +0530

    chore(project): initialize Spring Boot project with dependencies

---
+++ src/main/java/org/bookstoreecommerce/BookstoreEcommerceApplication.java
@@ -0,0 +1,13 @@
+package org.bookstoreecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreEcommerceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/bookstoreecommerce/BookstoreEcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.bookstoreecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreEcommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
