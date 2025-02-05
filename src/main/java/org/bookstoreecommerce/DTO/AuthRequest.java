a3e0c0f377acbfae6a9d2f6892c041c967480ddf
shobikan <shobikanv.21@uom.lk>
2025-02-05 19:32:32 +0530

    feat(auth): add AuthRequest and AuthResponse

---
+++ src/main/java/org/bookstoreecommerce/DTO/AuthRequest.java
@@ -0,0 +1,11 @@
+package org.bookstoreecommerce.DTO;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class AuthRequest {
+    private String email;
+    private String password;
+}
