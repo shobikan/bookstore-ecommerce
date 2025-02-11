82f20cd6399cce37f46293d81005c7a8777511ae
shobikan <shobikanv.21@uom.lk>
2025-02-12 00:08:01 +0530

    feat(order): add ShippingDetailsRepository

---
+++ src/main/java/org/bookstoreecommerce/repository/ShippingDetailsRepository.java
@@ -0,0 +1,11 @@
+package org.bookstoreecommerce.repository;
+
+import org.bookstoreecommerce.entity.ShippingDetails;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface ShippingDetailsRepository extends JpaRepository<ShippingDetails, Long> {
+    @Query("SELECT sd FROM ShippingDetails sd WHERE sd.order.orderId = :orderId")
+    ShippingDetails findShippingDetailsByOrderId(Long orderId);
+
+}
