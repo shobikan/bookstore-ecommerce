5c70a74d3d6906ac63eafb7baf4610831e0d39a9
shobikan <shobikanv.21@uom.lk>
2025-02-16 01:11:10 +0530

    feat(auth): allow public access to forgotPassword endpoints

--- src/main/java/org/bookstoreecommerce/config/SecurityConfig.java
+++ src/main/java/org/bookstoreecommerce/config/SecurityConfig.java
@@ -25,7 +25,7 @@ private final AuthenticationProvider authenticationProvider;
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/api/v1/auth/**").permitAll()
+                        .requestMatchers("/api/v1/auth/**", "/api/v1/forgotPassword/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session
