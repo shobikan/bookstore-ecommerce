8c6a9ce6f0827ab7c81d0e0b5e4e0f36ed41775d
shobikan <shobikanv.21@uom.lk>
2025-02-15 14:59:20 +0530

    feat(mail): implement EmailService

---
+++ src/main/java/org/bookstoreecommerce/service/EmailService.java
@@ -0,0 +1,63 @@
+package org.bookstoreecommerce.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.bookstoreecommerce.DTO.EmailRequest;
+import org.bookstoreecommerce.entity.EmailLog;
+import org.bookstoreecommerce.repository.EmailLogRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+    private final JavaMailSender mailSender;
+    private final EmailLogRepository emailLogRepository;
+
+    @Value("${spring.mail.username}")
+    private String from;
+
+    public Boolean sendEmailWithLog(EmailRequest request) {
+        try {
+            sendMail(request);
+            saveEmailLog(request, "SENT");
+            return true;
+        } catch (MessagingException e) {
+            saveEmailLog(request, "FAILED");
+            throw new RuntimeException("Failed to send email", e);
+        }
+    }
+
+    private Boolean sendEmailSimple(EmailRequest request) {
+        try {
+            sendMail(request);
+            return true;
+        } catch (MessagingException e) {
+            throw new RuntimeException("Failed to send email", e);
+        }
+    }
+
+    private void sendMail (EmailRequest request) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom(from);
+        helper.setTo(request.getTo());
+        helper.setSubject(request.getSubject());
+        helper.setText(request.getBody(), true);
+
+        mailSender.send(message);
+    }
+
+    private void saveEmailLog(EmailRequest request, String status) {
+        EmailLog emailLog = new EmailLog();
+        emailLog.setRecipient(request.getTo());
+        emailLog.setSubject(request.getSubject());
+        emailLog.setBody(request.getBody());
+        emailLog.setStatus(status);
+        emailLogRepository.save(emailLog);
+    }
+}
